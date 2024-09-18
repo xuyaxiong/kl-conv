57056958e6b15d4d784458529d2a8e29c1df27b3
xuyaxiong <xyxlindy@163.com>
2024-09-18 17:03:10 +0800

    添加复制结果到剪贴板按钮

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import scrolledtext
+from tkinter import scrolledtext, messagebox
 from src import VERSION
 from src.lib.utils import convert
 
@@ -8,6 +8,13 @@ def submit():
     input_text = input_text_area.get("1.0", tk.END).strip()
     output_text_area.delete("1.0", tk.END)
     output_text_area.insert(tk.END, convert(input_text) + "\n")
+
+
+def copy():
+    output_text = output_text_area.get("1.0", tk.END).strip()
+    root.clipboard_clear()
+    root.clipboard_append(output_text)
+    messagebox.showinfo("Tips", "Copied to clipboard!")
 
 
 root = tk.Tk()
@@ -30,8 +37,16 @@ output_text_area = scrolledtext.ScrolledText(frame, width=50, height=10)
 output_text_area.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 output_text_area.pack_propagate(False)
 
+# 控制面板
+control_frame = tk.Frame(root)
+control_frame.pack()
+
 # 提交按钮
-submit_button = tk.Button(root, text="转换", command=submit)
-submit_button.pack(pady=10)
+submit_button = tk.Button(control_frame, text="转换", command=submit)
+submit_button.pack(side=tk.LEFT, padx=10)
+
+# 复制转换结果按钮
+copy_button = tk.Button(control_frame, text="复制", command=copy)
+copy_button.pack(side=tk.LEFT, padx=10)
 
 root.mainloop()

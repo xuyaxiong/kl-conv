14b65228bb8df9fc37b53f0918992deac8aadbb3
xu yaxiong <xyxlindy@163.com>
2024-10-02 12:27:24 +0800

    重构代码

--- src/app.py
+++ src/app.py
@@ -1,19 +1,10 @@
-import argparse
 from src.mode.gui import run_gui_mode
-from src.mode.cli import run_cli_mode
+from src.mode.cli import run_cli_mode, get_args
 from src.mode.serve import run_serve_mode
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--cli", action="store_true", help="start in command-line mode")
-    parser.add_argument(
-        "--docstrings", type=str, help="the docstrings you want to convert"
-    )
-    parser.add_argument("-f", type=str, help="the header file path you want to convert")
-    parser.add_argument("-o", type=str, help="the output file path")
-    parser.add_argument("-n", type=str, help="the output file name")
-    args = parser.parse_args()
+    args = get_args()
     if args.cli:
         run_cli_mode(args)
     else:
--- src/mode/cli.py
+++ src/mode/cli.py
@@ -1,4 +1,17 @@
+import argparse
 from src.lib.utils import convert, convert_file
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cli", action="store_true", help="start in command-line mode")
+    parser.add_argument(
+        "--docstrings", type=str, help="the docstrings you want to convert"
+    )
+    parser.add_argument("-f", type=str, help="the header file path you want to convert")
+    parser.add_argument("-o", type=str, help="the output file path")
+    parser.add_argument("-n", type=str, help="the output file name")
+    return parser.parse_args()
 
 
 def run_cli_mode(args):
@@ -9,3 +22,8 @@ def run_cli_mode(args):
         output_path = args.o or "."
         output_name = args.n or "xxx"
         convert_file(input_path, output_path, output_name)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    run_cli_mode(args)
